619ce64c764f551699ba32afd04e2ac0747b8ee0
NANSIYV <39900381+nsv2051@users.noreply.github.com>
2023-05-13 00:13:06 +0800

    Update add_ssh_key.sh

--- add_ssh_key.sh
+++ add_ssh_key.sh
@@ -1,46 +1,34 @@
 #!/bin/bash
 
 # 检查 SSH 目录是否存在，如果不存在则创建它
+
 if [ ! -d ~/.ssh ]; then
+
   mkdir ~/.ssh
+
 fi
 
 # 获取当前主机名并将其添加到 /etc/hosts 文件中
+
 HOSTNAME=$(hostname)
+
 echo "127.0.0.1 $HOSTNAME" | sudo tee -a /etc/hosts > /dev/null
 
 # 下载 SSH 密钥并将其添加到 authorized_keys 文件中
+
 curl https://github.com/nsv2051.keys > ~/.ssh/authorized_keys
 
 # 更改 SSH 目录和 authorized_keys 文件的权限
+
 chmod 700 ~/.ssh
+
 chmod 600 ~/.ssh/authorized_keys
 
 # 在 SSH 配置文件中禁用密码验证
-sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
-# 重启 SSH 服务以应用更改
-sudo service ssh restart
-#!/bin/bash
-
-# 检查 SSH 目录是否存在，如果不存在则创建它
-if [ ! -d ~/.ssh ]; then
-  mkdir ~/.ssh
-fi
-
-# 获取当前主机名并将其添加到 /etc/hosts 文件中
-HOSTNAME=$(hostname)
-echo "127.0.0.1 $HOSTNAME" | sudo tee -a /etc/hosts > /dev/null
-
-# 下载 SSH 密钥并将其添加到 authorized_keys 文件中
-curl https://github.com/nsv2051.keys > ~/.ssh/authorized_keys
-
-# 更改 SSH 目录和 authorized_keys 文件的权限
-chmod 700 ~/.ssh
-chmod 600 ~/.ssh/authorized_keys
-
-# 在 SSH 配置文件中禁用密码验证
 sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
 
 # 重启 SSH 服务以应用更改
+
 sudo service ssh restart
+
